91e395497ec5894aa80a6be2dbf8b9a9b201688f
Molusoj <molusoj@gmail.com>
2020-04-23 13:34:31 +0100

    kimberly portfolio

---
+++ js/main.js
@@ -0,0 +1,91 @@
+/*
+	Visualize by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+$(function() {
+
+	// Vars.
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#wrapper');
+
+	// Breakpoints.
+		skel.breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	// Disable animations/transitions until everything's loaded.
+		$body.addClass('is-loading');
+
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+		});
+
+	// Poptrox.
+		$window.on('load', function() {
+
+			$('.thumbnails').poptrox({
+				onPopupClose: function() { $body.removeClass('is-covered'); },
+				onPopupOpen: function() { $body.addClass('is-covered'); },
+				baseZIndex: 10001,
+				useBodyOverflow: false,
+				usePopupEasyClose: true,
+				overlayColor: '#000000',
+				overlayOpacity: 0.75,
+				popupLoaderText: '',
+				fadeSpeed: 500,
+				usePopupDefaultStyling: false,
+				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
+			});
+
+		});
+
+		
+
+});
+
+$(function(){
+  
+    var swiper = new Swiper('.carousel-gallery .swiper-container', {
+      effect: 'slide',
+      speed: 900,
+      slidesPerView: 5,
+      spaceBetween: 20,
+      simulateTouch: true,
+      autoplay: {
+        delay: 5000,
+        stopOnLastSlide: false,
+        disableOnInteraction: false
+      },
+      pagination: {
+        el: '.carousel-gallery .swiper-pagination',
+        clickable: true
+      },
+      breakpoints: {
+        // when window width is <= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 5
+        },
+        // when window width is <= 480px
+        425: {
+          slidesPerView: 2,
+          spaceBetween: 10
+        },
+        // when window width is <= 640px
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
+    }); /*http://idangero.us/swiper/api/*/
+  
+    
+  
+});
